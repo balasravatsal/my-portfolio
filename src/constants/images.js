3c94af0405266d0323de4021714d6b664fa5e245
Vatsal <vatsalbalasra24@gmail.com>
2024-05-24 14:37:11 +0530

    images updated

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,6 @@
 import email from '../assets/email.png';
 import mobile from '../assets/mobile.png';
-import api from '../assets/api.png';
+// import api from '../assets/api.png';
 import cpp from '../assets/cpp.png';
 import css from '../assets/css.png';
 import figma from '../assets/figma.png';
@@ -23,29 +23,29 @@ import betterReact from '../assets/react-2.svg'
 import linkedin from '../assets/linkedin.svg'
 import resume from '../assets/document.png'
 
-import about01 from '../assets/about01.png';
-import about02 from '../assets/about02.png';
-import about03 from '../assets/about03.png';
-import about04 from '../assets/about04.png';
+// import about01 from '../assets/about01.png';
+// import about02 from '../assets/about02.png';
+// import about03 from '../assets/about03.png';
+// import about04 from '../assets/about04.png';
 
 import profile from '../assets/profile.png';
 import circle from '../assets/circle.svg';
 import logo from '../assets/logo.png';
 import myPic from '../assets/my-pic.png'
 
-import adidas from '../assets/adidas.png';
-import amazon from '../assets/amazon.png';
-import asus from '../assets/asus.png';
-import bolt from '../assets/bolt.png';
+// import adidas from '../assets/adidas.png';
+// import amazon from '../assets/amazon.png';
+// import asus from '../assets/asus.png';
+// import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
-import codechef from '../assets/codechef.png';
+import codechef from '../assets/codechef.svg';
 
 export default {
   email,
   mobile,
-  api,
+  // api,
   cpp,
   css,
   figma,
@@ -62,17 +62,17 @@ export default {
   sass,
   typescript,
   vue,
-  about01,
-  about02,
-  about03,
-  about04,
+  // about01,
+  // about02,
+  // about03,
+  // about04,
   profile,
   circle,
   logo,
-  adidas,
-  amazon,
-  asus,
-  bolt,
+  // adidas,
+  // amazon,
+  // asus,
+  // bolt,
   nb,
   skype,
   spotify,
